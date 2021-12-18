c35fd490682ea0027deaff23d5ef9474d336cd0d
kilimnik <mail@kilimnik.de>
2021-12-18 21:51:39 +0100

    install script fix partitions

--- install.sh
+++ install.sh
@@ -7,18 +7,20 @@ help() {
     exit 0
 }
 
-if [ -z "$1" ]; then
+disk=$1
+
+if [ -z "$disk" ]; then
     help
 fi
 
-parted --list 2>/dev/null | grep -w "$1" >/dev/null
+parted --list 2>/dev/null | grep -w "$disk" >/dev/null
 if [[ $? -ne 0 ]]; then
     echo "No such drive."
     exit 1
 fi
 
-parted $1 print
-read -r -p "Are you sure you want to use $1? [y/N] (The drive will be formatted and the data lost) " response
+parted $disk print
+read -r -p "Are you sure you want to use $disk? [y/N] (The drive will be formatted and the data lost) " response
 response=${response,,} # tolower
 if [[ ! $response =~ ^(yes|y) ]] || [[ -z $response ]]; then
     echo "Canceled"
@@ -27,25 +29,27 @@ fi
 
 # Ram
 RAM=$(free --gibi | head -2 | tail -1 | awk '{print $2}')
-RAM=$(echo $RAM | awk '{print $1 + int(sqrt($1))}')
+RAM=$(echo $RAM | awk '{print $disk + int(sqrt($disk))}')
 RAM=$(echo $RAM | awk '{print int($0+1)}')
 RAM=$(printf "%.0f" $RAM)
 
 # Table
-parted -s $1 mklabel "gpt"
+parted -s $disk mklabel "gpt"
 
 # Boot Parition
-parted -s $1 mkpart "ESP" "fat32" "1M" "1024MiB"
-parted -s $1 set 1 boot on
-mkfs.vfat "$11"
+parted -s $disk mkpart "ESP" "fat32" "1M" "1024MiB"
+parted -s $disk set 1 boot on
+partition1=$(lsblk -np --output KNAME | grep "^$disk.1\$")
+mkfs.vfat "$partition1"
 
 # Crypt lvm
-parted -s $1 mkpart "crypt" "1024MiB" "100%"
+parted -s $disk mkpart "crypt" "1024MiB" "100%"
+partition2=$(lsblk -np --output KNAME | grep "^$disk.2\$")
 
 dd if=/dev/urandom of=./keyfile-vgNix.bin bs=1024 count=4
-cryptsetup luksFormat -q --type luks1 -c aes-xts-plain64 -s 256 -h sha512 "$12"
-cryptsetup luksAddKey -q "$12" keyfile-vgNix.bin
-cryptsetup luksOpen -q "$12" cryptlvm -d keyfile-vgNix.bin
+cryptsetup luksFormat -q --type luks1 -c aes-xts-plain64 -s 256 -h sha512 "$partition2"
+cryptsetup luksAddKey -q "$partition2" keyfile-vgNix.bin
+cryptsetup luksOpen -q "$partition2" cryptlvm -d keyfile-vgNix.bin
 
 pvcreate /dev/mapper/cryptlvm
 vgcreate vgNix /dev/mapper/cryptlvm
@@ -58,7 +62,7 @@ mkfs.ext4 -L root /dev/mapper/vgNix-root
 
 mount /dev/vgNix/root /mnt
 mkdir -p /mnt/boot/efi
-mount "$11" /mnt/boot/efi
+mount "$partition1" /mnt/boot/efi
 swapon /dev/vgNix/swap
 
 mkdir -p /mnt/etc/secrets/initrd/
